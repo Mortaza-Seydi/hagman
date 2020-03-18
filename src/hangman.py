dbaa123448e4ed2ec7f6d6fcf7025a9536a08ac6
Morteza Seydi <mortoozseydi@gmail.com>
2020-03-18 20:12:17 +0330

    خوندن از فایل بصورت تصادفی اوکی شد

--- src/hangman.py
+++ src/hangman.py
@@ -8,48 +8,48 @@ import hangman_lib
 WORDLIST_FILENAME = "words.txt"
 
 
-# def load_words():
-#     """
-#     Returns a list of valid words. Words are strings of lowercase letters.
-#
-#     Depending on the size of the word list, this function may
-#     take a while to finish.
-#     """
-#     print("Loading word list from file...")
-#     # inFile: file
-#     inFile = open(WORDLIST_FILENAME, 'r')
-#     # line: string
-#     line = inFile.readline()
-#     # wordlist: list of strings
-#     wordlist = line.split()
-#     print("  ", len(wordlist), "words loaded.")
-#     print('Enter play_hangman() to play a game of hangman!')
-#     return wordlist
-#
-#
-# # actually load the dictionary of words and point to it with
-# # the words_dict variable so that it can be accessed from anywhere
-# # in the program
-# words_dict = load_words()
-#
-#
-# # Run get_word() within your program to generate a random secret word
-# # by using a line like this within your program:
-# # secret_word = get_word()
-#
-# def get_word():
-#     """
-#     Returns a random word from the word list
-#     """
-#     word = words_dict[randrange(0, len(words_dict))]
-#     return word
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r')
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = line.split()
+    print("  ", len(wordlist), "words loaded.")
+    # print('Enter play_hangman() to play a game of hangman!')
+    return wordlist
+
+
+# actually load the dictionary of words and point to it with
+# the words_dict variable so that it can be accessed from anywhere
+# in the program
+words_dict = load_words()
+
+
+# Run get_word() within your program to generate a random secret word
+# by using a line like this within your program:
+# secret_word = get_word()
+
+def get_word():
+    """
+    Returns a random word from the word list
+    """
+    word = words_dict[randrange(0, len(words_dict))]
+    return word
 
 
 # CONSTANTS
 MAX_GUESSES = 6
 
 # GLOBAL VARIABLES 
-secret_word = 'claptrap'
+secret_word = ''
 letters_guessed = []
 mistakes_made = 0
 letter_domain = list(string.ascii_lowercase)
@@ -120,12 +120,13 @@ def play_hangman():
     global secret_word
     global letters_guessed
     global guessed_word
+    global mistakes_made
     # Put the mistakes_made variable here, since you'll only use it in this function
 
     result = False
 
     # Update secret_word.
-    # secret_word  = get_word()
+    secret_word = get_word()
 
     for _ in secret_word:
         guessed_word += '-'
